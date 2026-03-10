433e6f252e9ca8913af1b855fb6d2f626cb260c2
Raj Vishwakarma <rajrv6929@gmail.com>
2026-03-10 13:31:39 +0530

    Implement Matching Order class and submit order function

---
+++ include/sequitur/core/MatchingEngine.hpp
@@ -0,0 +1,24 @@
+#include <memory>
+#include <sequitur/core/OrderBook.hpp>
+#include <sequitur/memory/ObjectPool.hpp>
+
+using namespace sequitur::memory;
+
+namespace sequitur {
+namespace core {
+class MatchingEngine {
+private:
+  ObjectPool<Order> pool;
+  std::unique_ptr<OrderBook> book;
+  ExecutionQueue exec_q;
+  GarbageQueue trash_q;
+  uint64_t next_order_id;
+
+public:
+  MatchingEngine(size_t pool_capacity)
+      : pool(pool_capacity), book(std::make_unique<OrderBook>()),
+        next_order_id(1) {}
+  void submit_order(uint8_t side, uint64_t price, uint32_t quantity);
+};
+} // namespace core
+} // namespace sequitur
--- src/core/MatchingEngine.cpp
+++ src/core/MatchingEngine.cpp
@@ -1,1 +1,27 @@
-#include <sequitur/core/Order.hpp>
+#include <sequitur/core/MatchingEngine.hpp>
+
+namespace sequitur {
+namespace core {
+void MatchingEngine::submit_order(uint8_t side, uint64_t price,
+                                  uint32_t quantity) {
+  Order *order = pool.acquire();
+  order->side = side;
+  order->price = price;
+  order->quantity = quantity;
+  order->id = next_order_id++;
+
+  book->match_order(order, exec_q, trash_q);
+
+  for (auto *dead_order : trash_q) {
+    pool.release(dead_order);
+  }
+
+  trash_q.clear();
+
+  if (order->quantity == 0) {
+    pool.release(order);
+  }
+  exec_q.clear();
+}
+} // namespace core
+} // namespace sequitur
